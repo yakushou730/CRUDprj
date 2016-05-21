2855689ab960a94a255f39688e4bf967eb222736
YaoShangTseng <yakushou730@gmail.com>
2016-05-21 16:46:29 +0800

    Finish basic CRUD for book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,45 @@
 class BooksController < ApplicationController
+
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book.destroy
+
+    redirect_to books_path
+  end
+
+  private
+  def set_book
+    @book = Book.find(params[:id])
+  end
+  def book_params
+    params.require(:book).permit(:name, :description, :isbn)
   end
 end
